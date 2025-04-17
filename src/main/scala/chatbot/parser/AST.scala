37b128f67c50ab3d36c92f55f0b03feada192744
Mohamed Mostafa <82705935+SoloAdventurer@users.noreply.github.com>
2025-04-17 15:39:36 +0200

    Initial file structure with AST and config complete

--- src/main/scala/chatbot/parser/AST.scala
+++ src/main/scala/chatbot/parser/AST.scala
@@ -3,7 +3,12 @@ package chatbot.parser
 object AST {
   sealed trait Command
   object Command {
-    case object Help extends Command
-    // TODO: Add other command types like Ask, ListCategory, etc.
+    case object Help                   extends Command
+    case class AskAbout(topic: String) extends Command
+    case object ListCategory           extends Command
+    case object ListPlanets            extends Command
+    case object RandomFact             extends Command
+    case object StartQuiz              extends Command
+    case class Unknown(input: String)  extends Command
   }
 }
